ad156e7dfd9fa2c029533dc52f9ba22b6a5cc8a7
Mark Smith <mark@theflyingcodr.dev>
2021-05-14 14:26:48 +0100

    windows linter failing, lets see if this fixes it

--- bip32/extendedkey.go
+++ bip32/extendedkey.go
@@ -1,7 +1,6 @@
 // Copyright (c) 2014-2016 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
-
 package bip32
 
 // References:
